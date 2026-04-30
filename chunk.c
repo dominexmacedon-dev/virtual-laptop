b0b9be6f15fe2a072cd753e2805bc8763c2ecdac
dominexmacedon-dev <starlightprogramminglanguage@gmail.com>
2026-04-30 07:38:08 +0630

    Update chunk.c

--- chunk.c
+++ chunk.c
@@ -1,102 +1,37 @@
-#include "chunk.h"
 #include <stdlib.h>
-
-static void* reallocate(void* ptr, size_t oldSize, size_t newSize) {
-    if (newSize == 0) {
-        free(ptr);
-        return NULL;
-    }
-    return realloc(ptr, newSize);
-}
-
-static int grow(int capacity) {
-    return capacity < 8 ? 8 : capacity * 2;
-}
+#include "chunk.h"
 
 void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
-
     chunk->lines = NULL;
+    initValueArray(&chunk->constants);
+}
 
-    chunk->constantsCount = 0;
-    chunk->constantsCapacity = 0;
-    chunk->constants = NULL;
+void writeChunk(Chunk* chunk, uint8_t byte, int line) {
+    if (chunk->capacity < chunk->count + 1) {
+        int oldCapacity = chunk->capacity;
+        chunk->capacity = oldCapacity < 8 ? 8 : oldCapacity * 2;
+
+        chunk->code = realloc(chunk->code, sizeof(uint8_t) * chunk->capacity);
+        chunk->lines = realloc(chunk->lines, sizeof(int) * chunk->capacity);
+    }
+
+    chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
+    chunk->count++;
+}
+
+int addConstant(Chunk* chunk, Value value) {
+    writeValueArray(&chunk->constants, value);
+    return chunk->constants.count - 1;
 }
 
 void freeChunk(Chunk* chunk) {
     free(chunk->code);
     free(chunk->lines);
-    free(chunk->constants);
+    freeValueArray(&chunk->constants);
 
-    chunk->count = 0;
-    chunk->capacity = 0;
-    chunk->code = NULL;
-
-    chunk->lines = NULL;
-
-    chunk->constantsCount = 0;
-    chunk->constantsCapacity = 0;
-    chunk->constants = NULL;
-}
-
-void writeChunk(Chunk* chunk, uint8_t byte) {
-    if (chunk->count + 1 > chunk->capacity) {
-        int old = chunk->capacity;
-        chunk->capacity = grow(old);
-
-        chunk->code = (uint8_t*)reallocate(
-            chunk->code,
-            old * sizeof(uint8_t),
-            chunk->capacity * sizeof(uint8_t)
-        );
-
-        chunk->lines = (int*)reallocate(
-            chunk->lines,
-            old * sizeof(int),
-            chunk->capacity * sizeof(int)
-        );
-    }
-
-    chunk->code[chunk->count] = byte;
-    chunk->lines[chunk->count] = 0;
-    chunk->count++;
-}
-
-int addConstant(Chunk* chunk, Value value) {
-    if (chunk->constantsCount + 1 > chunk->constantsCapacity) {
-        int old = chunk->constantsCapacity;
-        chunk->constantsCapacity = grow(old);
-
-        chunk->constants = (Value*)reallocate(
-            chunk->constants,
-            old * sizeof(Value),
-            chunk->constantsCapacity * sizeof(Value)
-        );
-    }
-
-    chunk->constants[chunk->constantsCount] = value;
-    return chunk->constantsCount++;
-}
-
-Value intVal(int v) {
-    Value val;
-    val.type = VAL_INT;
-    val.as.i = v;
-    return val;
-}
-
-Value floatVal(float v) {
-    Value val;
-    val.type = VAL_FLOAT;
-    val.as.f = v;
-    return val;
-}
-
-Value boolVal(int v) {
-    Value val;
-    val.type = VAL_BOOL;
-    val.as.b = v;
-    return val;
+    initChunk(chunk);
 }
