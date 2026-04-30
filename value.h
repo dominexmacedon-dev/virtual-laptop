2d01355c68e4ca3bddece04f8539160fb69bc178
dominexmacedon-dev <starlightprogramminglanguage@gmail.com>
2026-04-30 07:34:44 +0630

    Update value.h

--- value.h
+++ value.h
@@ -1,28 +1,55 @@
-#ifndef VALUE_H
-#define VALUE_H
+#ifndef value_h
+#define value_h
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "common.h"
+
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
 
 typedef enum {
-    VAL_INT,
-    VAL_FLOAT,
     VAL_BOOL,
-    VAL_NULL
+    VAL_NIL,
+    VAL_NUMBER,
+    VAL_OBJ
 } ValueType;
 
 typedef struct {
     ValueType type;
     union {
-        int i;
-        float f;
-        int b;
+        bool boolean;
+        double number;
+        Obj* obj;
     } as;
 } Value;
 
-Value intVal(int v);
-Value floatVal(float v);
-Value boolVal(int v);
-Value nullVal(void);
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
 
-int isFalse(Value v);
-int valuesEqual(Value a, Value b);
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+#define AS_OBJ(value)     ((value).as.obj)
+
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)object}})
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+
+bool valuesEqual(Value a, Value b);
+
+void printValue(Value value);
 
 #endif
