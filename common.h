923d7e211b4e5b8f9cbd4a676b14f5ce39390dcd
dominexmacedon-dev <starlightprogramminglanguage@gmail.com>
2026-04-30 07:33:25 +0630

    Update common.h

--- common.h
+++ common.h
@@ -1,78 +1,84 @@
-#ifndef COMMON_H
-#define COMMON_H
+#ifndef common_h
+#define common_h
 
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-typedef enum {
-    VAL_INT,
-    VAL_FLOAT,
-    VAL_BOOL,
-    VAL_NULL
-} ValueType;
+#define DEBUG_TRACE_EXECUTION 0
 
-typedef struct {
-    ValueType type;
-    union {
-        int i;
-        float f;
-        int b;
-    } as;
-} Value;
+typedef double Value;
 
 typedef enum {
 
     OP_CONSTANT,
+    OP_NIL,
+    OP_TRUE,
+    OP_FALSE,
+
+    OP_POP,
+    OP_DUP,
+
+    OP_GET_LOCAL,
+    OP_SET_LOCAL,
+
+    OP_GET_GLOBAL,
+    OP_DEFINE_GLOBAL,
+    OP_SET_GLOBAL,
+
+    OP_GET_UPVALUE,
+    OP_SET_UPVALUE,
 
     OP_ADD,
     OP_SUBTRACT,
     OP_MULTIPLY,
     OP_DIVIDE,
-    OP_MOD,
-    OP_POW,
     OP_NEGATE,
 
     OP_EQUAL,
-    OP_NOT_EQUAL,
     OP_GREATER,
-    OP_GREATER_EQUAL,
     OP_LESS,
-    OP_LESS_EQUAL,
 
     OP_NOT,
+
     OP_AND,
     OP_OR,
 
-    OP_BIT_AND,
-    OP_BIT_OR,
-    OP_BIT_XOR,
-    OP_BIT_NOT,
-    OP_SHIFT_LEFT,
-    OP_SHIFT_RIGHT,
-
-    OP_POP,
-    OP_DUP,
-    OP_SWAP,
-
-    OP_OUT,
-
-    OP_DEFINE_GLOBAL,
-    OP_GET_GLOBAL,
-    OP_SET_GLOBAL,
-
-    OP_GET_LOCAL,
-    OP_SET_LOCAL,
-
     OP_JUMP,
     OP_JUMP_IF_FALSE,
-    OP_JUMP_IF_TRUE,
     OP_LOOP,
 
     OP_CALL,
     OP_RETURN,
 
-    OP_NOP,
+    OP_CLOSURE,
+    OP_CLOSE_UPVALUE,
+
+    OP_CLASS,
+    OP_METHOD,
+    OP_INVOKE,
+    OP_GET_PROPERTY,
+    OP_SET_PROPERTY,
+
+    OP_PRINT,
+
     OP_END
 
 } OpCode;
+
+#define UINT8_COUNT (UINT8_MAX + 1)
+
+#define ALLOCATE(type, count) \
+    (type*)realloc(NULL, sizeof(type) * (count))
+
+#define FREE(ptr) free(ptr)
+
+#if DEBUG_TRACE_EXECUTION
+#define DEBUG_PRINT(...) printf(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#endif
 
 #endif
